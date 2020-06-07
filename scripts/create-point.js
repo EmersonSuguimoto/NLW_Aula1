1ae2fd924b497fbcca5216f3213f355e44934058
suguieme <emerson.suguimoto@electrolux.com>
2020-06-07 14:37:10 -0300

    Aula 3 Create Point e Search Results

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,7 +1,7 @@
 function populateUFs(){
     const ufSelect = document.querySelector("select[name=uf]")
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
     .then( res => res.json() )
     .then( states => {
         for(const state of states){
@@ -33,7 +33,7 @@ function getcities(event){
     .then( cities => {
 
         for(const city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }        
 
         citySelect.disabled = false
@@ -43,3 +43,44 @@ function getcities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getcities)
+
+
+// Itens de Coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event){    
+        
+    const itemLi = event.target
+
+    // Add or Remove class in Javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( function(item) {
+        const itemFound = item == itemId
+        return itemFound
+    })
+    
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    }
+    else{
+        selectedItems.push(itemId)
+    }       
+
+    collectedItems.value = selectedItems
+}
